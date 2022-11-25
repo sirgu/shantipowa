c179c0ea699e2ef69305f5f5a60daf0e1c77cc08
max-elia <max_elia@hotmail.de>
2022-11-25 15:04:29 +0100

    CMS Migration
    Fetch all content from CMS
    Split site into components

---
+++ cms-migration/src/routes/Music.svelte
@@ -0,0 +1,115 @@
+<script>
+// @ts-nocheck
+
+import { onMount } from "svelte";
+export let music;
+
+onMount(()=>{
+    (function($){
+        
+$.fn.gridstackCarousel = function(){
+    $('.qt-gridstackCarousel').each(function(i,c){
+        var that = $(c),
+            w = that.width(),
+            h = w/16*7,
+            QTtime_constant = that.attr("data-time_constant"),
+            QTdist = that.attr("data-dist"),
+            QTshift = that.attr("data-shift"),
+            QTvpadding = that.attr("data-vpadding"),
+            QTfull_width = (that.attr("data-full_width") === "1" || that.attr("data-full_width") === "true"),
+            QTpadding = that.attr("data-padding");
+        if(QTtime_constant == undefined || QTtime_constant === "" ) {
+            QTtime_constant = 200;
+        }
+        if(QTdist == undefined || QTdist === "" ) {
+            QTdist = -30;
+        }
+        if(QTshift == undefined || QTshift === "" ) {
+            QTshift = 0;
+        }
+        if(QTpadding == undefined || QTpadding === "" ) {
+            QTpadding = 0;
+        }
+        if(QTvpadding == undefined || QTvpadding === "" ) {
+            QTvpadding = 0;
+        }
+        if(QTvpadding !== 0){
+            that.css({"margin-top":QTvpadding,"margin-bottom":QTvpadding});
+        } 
+        var atts = {
+            time_constant: parseInt(QTtime_constant, 10),
+            dist: parseInt(QTdist, 10),
+            padding: parseInt(QTpadding, 10),
+            shift:parseInt(QTshift, 10),
+            full_width: QTfull_width
+        };
+        that.carousel(atts);
+
+
+        that.parent().find(".prev").on("click",function(e){
+            e.preventDefault();
+            that.carousel("prev");
+        });
+        that.parent().find(".next").on("click",function(e){
+            e.preventDefault();
+            that.carousel("next");
+        });
+
+
+        that.find(".carousel-item").on("mouseenter touchstart", function(e){
+            var itemElem = $(this);
+            itemElem.addClass("active");
+            if($("body").hasClass("mobile")){
+                setTimeout(
+                function(){ 
+                    itemElem.removeClass("active"); 
+                } ,  3000);
+                that.find("a").on("touchstart", function(e){
+                    window.location.href = $(this).attr("href");
+                });
+            }
+        }).on("mouseleave", function(){
+            $(this).removeClass("active");
+        });
+    });
+}
+$.fn.gridstackCarousel();
+    })(jQuery);
+});
+</script>
+
+<div id="music" class="section section-music parallax-container scrollspy" >
+    <div class="parallax"><img src="images/parallax3.jpg" alt="background">
+    </div>
+    <h2 class="qt-section-title" data-100p-top="opacity:0;" data-80p-top="opacity:0;" data-50p-top="opacity:1;" data-30p-top="opacity:1;" data-8p-top="opacity:1;" data-0p-top="opacity:0;">MUSIC<i class="deco"></i></h2>
+    <div id="carouselmusic" class="qt-gridstackCarousel-container">
+        <div class="carousel qt-gridstackCarousel" data-vpadding="50" data-time_constant="500"   data-dist="-30"  data-shift="-20" data-padding="20" data-full_width="0" data-100p-top="opacity:0;" data-80p-top="opacity:0;" data-50p-top="opacity:1;" data-30p-top="opacity:1;" data-top-bottom="opacity:0;">
+            {#if music}
+            {#each music as song}
+
+            <div class="carousel-item">
+                <img src="{song.img.path}"  width="300" height="300" alt="{song.title}">
+                <h5>{song.title}</h5>
+                <a href="#modal1" class="btn modal-trigger" data-iframe="{song.ling}"><span class="lnr lnr-music-note"></span></a>
+            </div>
+            {/each}
+            {/if}
+        </div>
+        <div class="nav hide-on-med-and-down">
+            <!-- svelte-ignore a11y-missing-attribute -->
+            <a class="prev">
+                <span class="lnr lnr-arrow-left-circle"></span>
+            </a>
+            <!-- svelte-ignore a11y-missing-attribute -->
+            <a class="next">
+                <span class="lnr lnr-arrow-right-circle"></span>
+            </a>
+        </div>
+    </div>
+</div>
+
+<div id="modal1" class="modal bottom-sheet">
+    <div class="modal-content">
+        <iframe title="soundcloud" id="modalframe" class="qt-modalframe" src=""></iframe>
+    </div>
+</div>
---
+++ cms-migration/src/routes/Photos.svelte
@@ -0,0 +1,51 @@
+<script>
+// @ts-nocheck
+
+import { onMount } from "svelte";
+export let photos;
+
+onMount(()=>{
+    (function($){
+        $('.qtgallery').slick({
+            slidesToScroll: 1,
+            slidesToShow: 1,
+            infinite: true,
+            dots: true,
+            variableWidth: true,
+            arrows: true,
+            centerMode: true,
+            // centerPadding: '140px',
+            responsive: [
+                {
+                breakpoint: 480,
+                settings: {
+                    arrows: true,
+                    centerMode: false,
+                    // centerPadding: '40px',
+                    variableWidth: false,
+                    variableHeight: false,
+                    slidesToShow: 1,
+                    draggable: true,
+                    swipe: true,
+                    touchMove: true
+                }
+                }
+            ]
+        });
+        $('.qtgallery').slick('slickGoTo', 0);
+    })(jQuery);
+    
+});
+</script>
+<div id="photos" class="section section-photos black scrollspy">
+    <h2 class="qt-section-title" data-100p-top="opacity:0;" data-80p-top="opacity:0;" data-50p-top="opacity:1;">PHOTOS<i class="deco"></i></h2>
+    <div class="qtgallery"  data-bottom-top="opacity:0;" data-center-top="opacity:1;">
+        {#if photos}
+        {#each photos as photo}
+            <div><img src="{photo.img.path}" alt="{photo.title}"></div>
+        {/each}
+        {/if}
+
+            
+    </div>
+</div>
