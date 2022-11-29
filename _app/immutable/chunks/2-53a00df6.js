29c4658cda6fa34efbfa01f2c4ccd00aa18cdfc3
max-elia <max_elia@hotmail.de>
2022-11-29 10:00:10 +0100

    Use underscore to ignore sirgu and cms-migration folder on deploy

--- _app/immutable/chunks/2-53a00df6.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-434dd59d.js";export{t as component};
---
+++ app/immutable/chunks/1-5b66cc72.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-7f4798b3.js";export{t as component};
