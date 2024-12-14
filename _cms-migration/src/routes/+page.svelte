1af0aa70b54ad4d7a73f7c4b2382f2f443014af4
max-brothandwerk <max@julius-brantner.de>
2024-12-14 12:01:16 +0100

    Update background image

--- _cms-migration/src/routes/+page.svelte
+++ _cms-migration/src/routes/+page.svelte
@@ -211,7 +211,7 @@ onMount(async () => {
 </script>
 
 <header id="home" class="qt-header parallax-container scrollspy" >
-    <div class="parallax" data-start="" data-end=""><img src="img/opportunity.webp"  alt="background"></div>
+    <div class="parallax" data-start="" data-end=""><img src="img/boxx.jpg"  alt="background"></div>
     <div class="qt-valign-wrapper">
         <div class="qt-valign">
             <div class="container center-align">
--- app/immutable/chunks/2-bfcf129f.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-41450550.js";export{t as component};
